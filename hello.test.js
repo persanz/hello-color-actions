b63c114fc7f9da8cb37fdcb51dd1baee05459353
Pere LLuis Sanz <persanz@gmail.com>
2023-10-30 19:58:01 +0100

    commit

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello from the Dark side");
+    expect(hello.hello()).toEqual("Hello from the Dark Side");
   });
 });
